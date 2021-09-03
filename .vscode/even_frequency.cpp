f50b91df61ef94d1141630f3a098aad174d3591c
Gritide <celikdogukan2206@gmail.com>
2021-09-02 23:06:27 -0400

    new

---
+++ .vscode/even_frequency.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+bool solve(vector<int>& nums) {
+    //empty input and single input handler
+    int counter=1;
+    bool temp=true;
+    if(nums.size()==0 || nums.size()==1)
+    {
+        temp=false;
+    }
+
+    else{
+       sort(nums.begin(),nums.end());
+       for (int i=0;i<nums.size()-1;i++)
+       {
+           if(nums[i]==nums[i+1])
+           {
+               counter++;
+           }
+           else if(nums[i]!=nums[i+1]&& counter %2!=0)
+           {
+               temp=false;
+               break;
+ 
+           }
+           else{
+               counter=1;
+           }
+       }
+    }
+    return temp;
+    //itterate through array
+        //itterage through array
+
+}
+int main() {
+  vector<int> nums={1,3,1};
+  cout<<solve(nums);
+  return 0;
+}
